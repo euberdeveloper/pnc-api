eb0b19ca21273a5048b27fddba43b38d890a26ef
euberdeveloper <euberdeveloper@gmail.com>
2022-02-25 16:00:48 +0100

    fix(user.model): hide __v on results
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- source/models/user.model.ts
+++ source/models/user.model.ts
@@ -1,13 +1,23 @@
 import { Schema, model } from 'mongoose';
 import { User, UserRole } from '@/types/database';
 
-const schema = new Schema<User>({
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    role: { type: String, required: true, validate: (value: UserRole) => Object.values(UserRole).includes(value) },
-    creationDate: { type: Date, default: new Date() }
-});
+const schema = new Schema<User>(
+    {
+        username: { type: String, required: true, unique: true },
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        role: { type: String, required: true, validate: (value: UserRole) => Object.values(UserRole).includes(value) },
+        creationDate: { type: Date, default: new Date() }
+    },
+    {
+        toJSON: {
+            transform: (_doc, obj) => {
+                delete obj.__v;
+                return obj;
+            }
+        }
+    }
+);
 schema.virtual('id').get(function (this: any) {
     return this._id.toHexString();
 });
