f9d97a77ffa20a6e5e84e5ddd043735f62cc1cb5
euberdeveloper <euberdeveloper@gmail.com>
2022-02-25 16:24:34 +0100

    feat(learnwolds): add token retrieve
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- source/config/index.ts
+++ source/config/index.ts
@@ -54,6 +54,7 @@ const CONFIG = {
         }
     },
     LEARNWORLDS: {
+        API_ENDPOINT: process.env.LEARNWORLDS_API_ENDPOINT,
         CLIENT_ID: process.env.LEARNWORLDS_CLIENT_ID,
         CLIENT_SECRET: process.env.LEARNWORLDS_CLIENT_SECRET,
         GRANT_TYPE: process.env.LEARNWORLDS_GRANT_TYPE
--- source/services/learnworlds.service.ts
+++ source/services/learnworlds.service.ts
@@ -1,13 +1,38 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import axios from 'axios';
+
 import CONFIG from '@/config';
 
 interface LearnWorldsServiceOptions {
     learnworlds: typeof CONFIG.LEARNWORLDS;
 }
 
-export class LearnWorldsService {
-    constructor(private readonly options: LearnWorldsServiceOptions) {}
+export interface LearnWorldsToken {
+    access_token: string;
+    token_type: string;
+    expires_in: number;
+}
 
-    
+export class LearnWorldsService {
+    private readonly host: string;
+
+    constructor(private readonly options: LearnWorldsServiceOptions) {
+        this.host = this.options.learnworlds.API_ENDPOINT;
+    }
+
+    private async getToken(): Promise<LearnWorldsToken> {
+        return axios
+            .post(
+                `${this.host}/oauth2/access_token`,
+                {
+                    client_id: this.options.learnworlds.CLIENT_ID,
+                    client_secret: this.options.learnworlds.CLIENT_SECRET,
+                    grant_type: this.options.learnworlds.GRANT_TYPE
+                },
+                { headers: { 'Lw-Client': this.options.learnworlds.CLIENT_ID } }
+            )
+            .then(response => response.data);
+    }
 }
 
 export const userService = new LearnWorldsService({
